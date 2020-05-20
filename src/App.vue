704b984307cb1a75db7d4f7a644847186a5b7595
Je suis un dev <admin@jesuisundev.com>
2020-05-20 08:33:18 -0400

    init

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <callToAction
+      v-bind:message="messageCallToAction"
+    ></callToAction>
+
+    <photoGrid 
+      v-for="photo in photos" 
+      v-bind:photo="photo" 
+      v-bind:key="photo.id"
+    ></photoGrid>
+  </div>
+</template>
+
+<script>
+import callToAction from './components/callToAction.vue'
+import photoGrid from './components/photoGrid.vue'
+
+export default {
+  name: 'App',
+  components: {
+    callToAction,
+    photoGrid
+  },
+  data: () => {
+    return {
+      photos: [
+        {
+          id: 0,
+          url: "https://alumni.crg.eu/sites/default/files/default_images/default-picture_0_0.png"
+        },
+        {
+          id: 1,
+          url: "https://lh3.googleusercontent.com/proxy/f_2l9B-J5Z9pLLKyl_qzuj_LjFsJkCVX-G-GSq_wjlJ-wBaZBlremNaS_UHoHVjABmho0Dh82efQNhc1N1EBvB6X7lQz6ElKL1utFcwp5EY"
+        }
+      ],
+      messageCallToAction: "call to action"
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/photoGrid.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="photoGrid">
+    <img :src="photo.url" alt ="" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'photoGrid',
+  props: ['photo']
+}
+</script>
+
+<style scoped></style>
