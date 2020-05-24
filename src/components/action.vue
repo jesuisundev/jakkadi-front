e3b6ea51a813ccb14ed55eeb66618c9dbf89947a
Je suis un dev <admin@jesuisundev.com>
2020-05-24 09:48:49 -0400

    composants

---
+++ src/components/action.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="callToAction">
+    <div class="apps">
+      <h1>{{ message }}</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'callToAction',
+  props: {
+    message: String
+  }
+}
+</script>
+
+<style scoped>
+  .callToAction {
+    position: absolute;
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+    display: -webkit-box;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  h1 {
+    color: red;
+    background:black;
+    padding: 10px;
+  }
+</style>
